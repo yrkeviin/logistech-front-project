0ebf438adabceab213ccd7f1e2ef13f59abc55e6
LuizGabrielLopes <luiz.gabriellopesc@gmail.com>
2025-12-04 10:36:09 -0300

    feat: add responsivity

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,17 @@
 import './globals.css';
 
+export const metadata = {
+  title: 'Logistech - Sistema de Gestão Logística',
+  description: 'Sistema de gestão logística para entregas e motoristas',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=5',
+}
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="pt-BR">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+      </head>
       <body>{children}</body>
     </html>
   );
